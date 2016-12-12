6a05efd0907fd6e62dadcbb0ae1deecc85cfc1c6
E5440-BQRKL32\Administrator <simbahmso@yahoo.com>
2016-12-12 00:02:18 -0700

    Defining the Model
    Created classes that define model:
    Customer.cs - Order.cs - Products.cs - Orderitem.cs
    Also added System.ComponentModel.DataAnnotations as a reference to
    eCommerce.Model part of the project

---
+++ eCommerce.Model/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Model
+{
+    class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CustomerId { get; set; }
+        public ICollection<Orderitem> OrderItems { get; set; }
+    }
+}
---
+++ eCommerce.Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Model
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+        [MaxLength(255)]
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
