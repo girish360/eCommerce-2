8d871d6217428eb0b147a96b1ad594657404ef79
E5440-BQRKL32\Administrator <simbahmso@yahoo.com>
2016-12-12 02:51:02 -0700

    Update-database failing
    Error Number:-1,State:0,Class:20
    A network-related or instance-specific
    error occurred while establishing a connection to SQL Server

---
+++ eCommerce.DAL/Migrations/201612120944340_Initial.cs
@@ -0,0 +1,56 @@
+namespace eCommerce.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Orderitems",
+                c => new
+                    {
+                        OrderItemId = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Order_OrderId = c.Int(),
+                    })
+                .PrimaryKey(t => t.OrderItemId)
+                .ForeignKey("dbo.Orders", t => t.Order_OrderId)
+                .Index(t => t.Order_OrderId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        OrderDate = c.DateTime(nullable: false),
+                        CustomerId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Description = c.String(),
+                        ImageUrl = c.String(maxLength: 255),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Orderitems", "Order_OrderId", "dbo.Orders");
+            DropIndex("dbo.Orderitems", new[] { "Order_OrderId" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Orders");
+            DropTable("dbo.Orderitems");
+        }
+    }
+}
