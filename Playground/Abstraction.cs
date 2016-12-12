cadde9a549e73a0a555563ba962cd4b8ab641424
E5440-BQRKL32\Administrator <simbahmso@yahoo.com>
2016-12-12 01:36:27 -0700

    Data Repository - Abstraction / Separation from Controller
    Data Repository - Abstraction / Separation from Controller

---
+++ Playground/Abstraction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    public class Test {
+        public Test()
+        {
+            var dog = new Dog();
+            var cat = new Cat();
+
+            dog.Eyes = 2;
+            dog.Legs = 4;
+            dog.isMansBestFriend();
+        }
+    }
+
+    public class Dog
+    {
+        public int Legs { get; set; }
+        public int Eyes { get; set; }
+        public bool Loyal { get; set; }
+
+        public bool isMansBestFriend()
+        {
+            return Loyal;
+        }
+    }
+
+    internal class Cat
+    {
+        public int Legs { get; set; }
+        public int Eyes { get; set; }
+        public bool Loyal { get; set; }
+
+        public bool isMansBestFriend()
+        {
+            return Loyal;
+        }
+    }
+}
+
+    
