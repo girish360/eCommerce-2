dcd65f57e129819ae0059883c387f6213928816b
E5440-BQRKL32\Administrator <simbahmso@yahoo.com>
2016-12-12 21:50:30 -0700

    Fixed Inconsistent accessibility
    made base base abstract class public because it was not accessible, was
    gertting error:
    
    Inconsistent accessibility: base class 'RepositoryBase<Customer>' is
    less accessible than class 'CustomerRepository'

--- eCommerce.DAL/Repositories/CustomerRepository.cs
+++ eCommerce.DAL/Repositories/CustomerRepository.cs
@@ -1,19 +1,14 @@
 ﻿using eCommerce.DAL.Data;
+using eCommerce.DAL.Repositories;
 using eCommerce.Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace eCommerce.DAL.Repositories
 {
     public class CustomerRepository : RepositoryBase<Customer>
     {
-        public CustomerRepository(DataContext context) : base(context){
-            if (context == null)
-                throw new ArgumentException();
-
+        public CustomerRepository(DataContext context) : base(context)
+        {
         }
     }
 }
